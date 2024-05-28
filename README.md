debd07b2da5204dc8007dfdd707958463895154a
Menachem Rose <Menachemrose1@gmail.com>
2024-05-28 19:13:20 -0400

    added comments and cleaned code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This project creates a streamlit application, designed to display to the user the capabilities of image compression.
 
-In the application, the user can input a photo, and a Singular Value Decomposition is performed on the matrix of the photo. When a compression percentage is created, four photographs are displayed, one of which has been compressed to the selected percentage.
+In the application, the user can input a photo, and a Singular Value Decomposition is performed on the matrix of the photo. When a compression percentage is selected, four photographs are displayed, one of which has been compressed to the selected percentage.
 The user is then asked which photo is the compressed one and is informed if his selection is correct.
--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,16 @@ def random_sample(raw_image, processed_image):
 
 
 if __name__ == '__main__':
+    #link stylesheet and include introduction text
     st.markdown('<link rel="stylesheet" href=styles.css>', unsafe_allow_html=True)
     st.header('IMAGE COMPRESSION')
     st.markdown('<p style="color:blue">Welcome to this demonstration of image compression,'
                 ' using Singular Value Decomposition.</p>', unsafe_allow_html=True)
 
+    #include image picker
     image = st.file_uploader(label='Input an image to begin compression.', type=['png', 'jpg'])
 
+    #once image is selected, display image and chart depicting possible compression limits
     if image is not None:
         raw_image = PIL.Image.open(image)
         np_img = np.asarray(raw_image)
@@ -37,41 +40,22 @@ if __name__ == '__main__':
         st.subheader('Information Captured (%) vs Storage Space Used (%)')
         st.line_chart(data=df, x='Storage (%)')
 
+        # include percentage picker.
         perc = st.slider("Select a percentage for storage usage. "
                          "(The smaller the storage, the lower quality the compressed picture will have.)",
-                         1.0, 50.0, value=10.0, step=0.5)
+                         1.0, 100.0, value=10.0, step=0.5)
 
+        #process image and display choice
         proc_image.calculate_desired_rank(perc=perc)
         plotting = proc_image.add_ranks(svd_results=svd_results)
         sample = random_sample(raw_image, processed_image=plotting)
         st.markdown('One of the below pictures has been compressed. Which is it?')
-        col1, col2, col3, col4 = st.columns(4)
-        with col1:
-            st.image(image=sample[0], clamp=True)
-            if st.button('SELECT', key=0):
+        for index, column in enumerate(st.columns(4)):
+            with column:
+                st.image(image=sample[index], clamp=True)
+                if st.button('SELECT', key=index):
 
-                if type(sample[0]) == np.ndarray:
-                    st.markdown('<p style="color:green">CORRECT</p>', unsafe_allow_html=True)
-                else:
-                    st.markdown('<p style="color:red">INCORRECT</p>', unsafe_allow_html=True)
-        with col2:
-            st.image(image=sample[1], clamp=True)
-            if st.button('SELECT', key=1):
-                if type(sample[1]) == np.ndarray:
-                    st.markdown('<p style="color:green">CORRECT</p>', unsafe_allow_html=True)
-                else:
-                    st.markdown('<p style="color:red">INCORRECT</p>', unsafe_allow_html=True)
-        with col3:
-            st.image(image=sample[2], clamp=True)
-            if st.button('SELECT', key=2):
-                if type(sample[2]) == np.ndarray:
-                    st.markdown('<p style="color:green">CORRECT</p>', unsafe_allow_html=True)
-                else:
-                    st.markdown('<p style="color:red">INCORRECT</p>', unsafe_allow_html=True)
-        with col4:
-            st.image(image=sample[3], clamp=True)
-            if st.button('SELECT', key=3):
-                if type(sample[3]) == np.ndarray:
-                    st.markdown('<p style="color:green">CORRECT</p>', unsafe_allow_html=True)
-                else:
-                    st.markdown('<p style="color:red">INCORRECT</p>', unsafe_allow_html=True)
+                    if type(sample[index]) == np.ndarray:
+                        st.markdown('<p style="color:green">CORRECT</p>', unsafe_allow_html=True)
+                    else:
+                        st.markdown('<p style="color:red">INCORRECT</p>', unsafe_allow_html=True)
